c15ffa01cb29648fd9f13f1ead6919b4d3f44818
zhangliang7190 <zl7190@126.com>
2023-04-27 18:00:11 +0800

    pool address compute adapted with 0x41 instead of '0xff'

--- contracts/v3-periphery/contracts/libraries/PoolAddress.sol
+++ contracts/v3-periphery/contracts/libraries/PoolAddress.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.5.0;
 
 /// @title Provides functions for deriving a pool address from the factory, tokens, and the fee
 library PoolAddress {
-    bytes32 internal constant POOL_INIT_CODE_HASH = 0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54;
+    bytes32 internal constant POOL_INIT_CODE_HASH = 0xa7f107a82c63b27c703744318311489b5d22f97106648cb446d4668e40503091;
 
     /// @notice The identifying key of the pool
     struct PoolKey {
@@ -36,7 +36,7 @@ library PoolAddress {
             uint256(
                 keccak256(
                     abi.encodePacked(
-                        hex'ff',
+                        hex'41',
                         factory,
                         keccak256(abi.encode(key.token0, key.token1, key.fee)),
                         POOL_INIT_CODE_HASH
