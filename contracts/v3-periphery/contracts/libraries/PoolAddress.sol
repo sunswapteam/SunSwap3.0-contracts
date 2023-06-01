ee76b7c8869657e1be14c62cf2cec917f3f7ff57
zhangliang7190 <zl7190@126.com>
2023-06-01 11:20:34 +0800

    exchange new pool_init_code_hash

--- contracts/v3-periphery/contracts/libraries/PoolAddress.sol
+++ contracts/v3-periphery/contracts/libraries/PoolAddress.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.5.0;
 
 /// @title Provides functions for deriving a pool address from the factory, tokens, and the fee
 library PoolAddress {
-    bytes32 internal constant POOL_INIT_CODE_HASH = 0xa7f107a82c63b27c703744318311489b5d22f97106648cb446d4668e40503091;
+    bytes32 internal constant POOL_INIT_CODE_HASH = 0x5e147b345c77aa6fe23e29cdf5bf6e0999a936c789f3b7a1a6839be85881d45e;
 
     /// @notice The identifying key of the pool
     struct PoolKey {
